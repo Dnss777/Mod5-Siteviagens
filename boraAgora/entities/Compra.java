46a3596d26de245d430906fa8aeecbafc761aaf0
DENISE FERNANDA <140545608+Dnss777@users.noreply.github.com>
2023-12-17 17:14:26 -0300

    Destino e Cliente feitos ok
    Compra a fazer , devido a fk só está cadastrando no banco mas não está listando , excluindo e editando os formulários;

---
+++ boraAgora/entities/Compra.java
@@ -0,0 +1,37 @@
+package com.Mod5.boraAgora.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "compra")
+public class Compra{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String data;
+	private Double total;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_cli")
+	private Cliente cliente;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_dest")
+	private Destinos destino;}
+	
+
+
+
+
+
+
---
+++ boraAgora/repositories/DestinoRepository.java
@@ -0,0 +1,9 @@
+package com.Mod5.boraAgora.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Mod5.boraAgora.entities.Destinos;
+
+public interface DestinoRepository extends JpaRepository<Destinos, Long> {
+	
+}
